5e6be2f6a76ce5723ade1102e19bbaa8af310e24
Mostafa Mohamed Oraby <mostaaf.oraby159@gmail.com>
2024-04-18 17:37:26 +0200

    adding 1 page and edit in style

--- src/app/components/personal-info/personal-info.component.ts
+++ src/app/components/personal-info/personal-info.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-personal-info',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterModule],
+  imports: [ReactiveFormsModule, RouterModule, RouterLink,RouterLinkActive],
   templateUrl: './personal-info.component.html',
-  styleUrl: './personal-info.component.css'
+  styleUrl: './personal-info.component.css',
 })
-export class PersonalInfoComponent {
+export class PersonalInfoComponent implements OnInit {
+  
 
+  form = new FormGroup({
+    Name: new FormControl(),
+    email: new FormControl(),
+    phone: new FormControl(),
+  });
+
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      Name: ['', Validators.required],
+      email: ['', Validators.required, Validators.email],
+      phone: ['', Validators.required , Validators.maxLength(11)],
+    });
+
+  }
+  
 }
--- src/app/components/select-plan/select-plan.component.ts
+++ src/app/components/select-plan/select-plan.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, viewChild } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-select-plan',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule , ReactiveFormsModule , CommonModule,RouterLink,RouterLinkActive],
   templateUrl: './select-plan.component.html',
   styleUrl: './select-plan.component.css'
 })
 export class SelectPlanComponent {
+
 
 }
